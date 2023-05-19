491e474194b8cb5023e7ee79391dc69ddf59e273
Gustavo Henrique <84508964+guustavo07@users.noreply.github.com>
2023-05-19 11:23:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3vPEyxgajspV0Ba0zNm0QXpEZrrQ2C3U",
+  authDomain: "chatjs-34dec.firebaseapp.com",
+  projectId: "chatjs-34dec",
+  storageBucket: "chatjs-34dec.appspot.com",
+  messagingSenderId: "47695739319",
+  appId: "1:47695739319:web:69dc98b5da7ec77a84d032"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
